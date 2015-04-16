2163c0e72e6188c357e820b697081fd1683759b9
Toni Cafiero <toni@cafiero.it>
2015-04-16 10:36:22 +0200

    Documentazione

---
+++ MCP25020/CAN_IO_EXPANDER.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=CAN_IO_EXPANDER.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/CAN_IO_EXPANDER.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=canioexpander.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/canioexpander.x.tar
